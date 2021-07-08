ece9fbf8efc382e1f2bbf9f78b2d33bab6fea2db
MrMohebi <MMMohebi@outlook.com>
2021-07-08 17:14:45 +0430

    links collection migration

--- database/migrations/mongodb/2021_07_08_111916_create_links_table.php
+++ database/migrations/mongodb/2021_07_08_111916_create_links_table.php
@@ -15,6 +15,19 @@ class CreateLinksTable extends Migration
     {
         Schema::create('links', function (Blueprint $table) {
             $table->id();
+            $table->string("firstname");
+            $table->string("lastname");
+            $table->string("age");
+            $table->string("addressText");
+            $table->json("addressCoordinates");
+            $table->string("phone");
+            $table->string("gender");
+            $table->string("maritalStatus");
+            $table->json("testResult");
+            $table->boolean("isEditable");
+            $table->timestamp("lastEditAt");
+            $table->timestamp("from");
+            $table->timestamp("to");
             $table->timestamps();
         });
     }
