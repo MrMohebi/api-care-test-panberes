8530c34d736a2bd023fdc6c7d97698cbc1d340d8
MrMohebi <MMMohebi@outlook.com>
2021-07-08 19:20:58 +0430

    add code to links collection

--- database/migrations/mongodb/2021_07_08_111916_create_links_table.php
+++ database/migrations/mongodb/2021_07_08_111916_create_links_table.php
@@ -15,6 +15,7 @@ class CreateLinksTable extends Migration
     {
         Schema::create('links', function (Blueprint $table) {
             $table->id();
+            $table->string("code");
             $table->string("firstname");
             $table->string("lastname");
             $table->string("age");
