d0fda4ed908c9191cae29ac5c27dfcc268da7c74
MrMohebi <MMMohebi@outlook.com>
2021-07-09 23:34:53 +0430

    create link

---
+++ app/GraphQL/Mutations/CreateLink.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\GraphQL\Mutations;
+
+use App\Models\Link;
+use App\Models\User;
+
+
+class CreateLink
+{
+    /**
+     * @param  null  $_
+     * @param  array<string, mixed>  $args
+     */
+    public function __invoke($_, array $args):Link{
+        $user = User::where("token", $args["token"])->first();
+        if(!isset($user->id)){
+            $emptyLink = new Link;
+            $emptyLink->id = "token is not valid!";
+            return $emptyLink;
+        }
+        $link = Link::create($args);
+        $link->code = self::randomString(5);
+        $link->testResult = array();
+        $link->creatorId = $user->id;
+        $link->creatorName = $user->firstname . " " . $user->lastname;
+        $link->save();
+
+        return $link;
+    }
+
+    private static function randomString(int $length = 10):string {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
+}
