052f1e89eab2a321b17b4cfbfed53c10fe3e7402
MrMohebi <MMMohebi@outlook.com>
2021-07-10 12:47:37 +0430

    return user intense

--- app/GraphQL/Queries/Login.php
+++ app/GraphQL/Queries/Login.php
@@ -10,9 +10,7 @@ class Login
      * @param  null  $_
      * @param  array<string, mixed>  $args
      */
-    public function __invoke($_, array $args):array{
-        $returnFields = ["token"=>"", "firstname"=>"", "lastname"=>""];
-
+    public function __invoke($_, array $args):User{
         $username = $args["username"];
         $password = $args["password"];
         $user =  User::where("username", $username)->first();
@@ -21,12 +19,13 @@ class Login
                 $token = self::randomStringLower(32);
                 $user->token = $token;
                 $user->save();
-                $returnFields["token"] = $token;
-                return $returnFields;
+                unset($user->password);
+                return $user;
             }
         }
-        $returnFields["token"] = "username or password is incorrect";
-        return $returnFields;
+        $emptyUser = new User;
+        $emptyUser->token = "username or password is incorrect";
+        return $emptyUser;
     }
     private static function randomStringLower(int $length = 10):string {
         $characters = '0123456789abcdefghijklmnopqrstuvwxyz';
