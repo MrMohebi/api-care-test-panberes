4ac16a691a9edd06d2a62cf5dd5836f565bde25c
MrMohebi <MMMohebi@outlook.com>
2021-07-22 19:26:59 +0430

    createCustomer

---
+++ app/GraphQL/Mutations/CreateCustomer.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\GraphQL\Mutations;
+
+use App\Models\Customer;
+use App\Models\User;
+
+class CreateCustomer{
+    public function __invoke($_, array $args):Customer{
+        $user = User::where("token", $args["token"])->first() ?? new User;
+        $customer = Customer::create($args);
+        $customer->marketerId = $user->id;
+        $customer->marketerName = $user->firstname . " " . $user->lastname;
+        $customer->save();
+        return $customer;
+    }
+}
