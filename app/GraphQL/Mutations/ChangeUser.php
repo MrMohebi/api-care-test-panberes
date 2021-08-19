d62c566337cf69c45ac2d3905f9c3940900c1076
MrMohebi <MMMohebi@outlook.com>
2021-08-19 15:03:04 +0430

    change user password

--- app/GraphQL/Mutations/ChangeUser.php
+++ app/GraphQL/Mutations/ChangeUser.php
@@ -3,11 +3,22 @@
 namespace App\GraphQL\Mutations;
 
 use App\Models\User;
+use GraphQL\Error\Error;
 
 class ChangeUser
 {
     public function __invoke($_, array $args):User{
         $user = User::where("token", $args["token"])->first();
+
+        if(isset($args["password"])){
+            if(isset($args["oldPassword"]) && password_verify($args["oldPassword"], $user->password)){
+                $user->password = password_hash($args["password"],PASSWORD_DEFAULT);
+                $user->save();
+            }else {
+                throw new Error("oldPassword in not correct");
+            }
+        }
+
         $user->update($args);
         return $user;
     }
