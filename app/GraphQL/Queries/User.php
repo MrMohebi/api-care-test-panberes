d7721a74e90bdd3a776f254c498f7e21f8497249
MrMohebi <MMMohebi@outlook.com>
2021-09-16 15:48:55 +0430

    fix fetch user

--- app/GraphQL/Queries/User.php
+++ app/GraphQL/Queries/User.php
@@ -6,10 +6,8 @@ class User{
     public function __invoke($_, array $args){
         $user = \App\Models\User::where("token", $args["token"])->first();
 
-
-
         $subsets = [];
-        foreach ($user->subsetsId as $eSubsetId){
+        foreach ($user->subsetsId ?? [] as $eSubsetId){
             $subsets[] = \App\Models\User::find($eSubsetId);
         }
         $user->subsets = $subsets;
